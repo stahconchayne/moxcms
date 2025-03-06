11a4fc4f4a19aa2c6d42794fca94f9969dd85e84
stahconchayne <5987019+stahconchayne@users.noreply.github.com>
2025-03-06 13:40:41 +0000

    Guess bottleneck

--- src/conversions/rgbxyz.rs
+++ src/conversions/rgbxyz.rs
@@ -243,14 +243,14 @@ where
         dst: &mut [T],
         working_set: &mut [f32; 12012],
     ) -> Result<(), CmsError> {
-        // let linear_fn = self.linear_search.as_ref();
-        // linear_fn(
-        //     src,
-        //     working_set,
-        //     &self.profile.r_linear,
-        //     &self.profile.g_linear,
-        //     &self.profile.b_linear,
-        // );
+        let linear_fn = self.linear_search.as_ref();
+        linear_fn(
+            src,
+            working_set,
+            &self.profile.r_linear,
+            &self.profile.g_linear,
+            &self.profile.b_linear,
+        );
 
         let cap_values = (GAMMA_LUT - 1) as f32;
 
